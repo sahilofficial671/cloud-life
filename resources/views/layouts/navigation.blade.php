550e71387153e2697af4ab072e0834636cbddc91
Sahil Bhatia <sahilofficial671@gmail.com>
2021-06-17 22:55:00 +0530

    Home page done!

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,22 +1,38 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white shadow-sm border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('index') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600"/>
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
+                {{-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('finance')" :active="Str::startsWith(request()->route()->getName(), 'finance')">
+                        {{ __('Finance') }}
+                    </x-nav-link>
+                </div> --}}
+            </div>
+
+            @guest
+            <div class="flex">
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                        {{ __('Login') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                        {{ __('Register') }}
                     </x-nav-link>
                 </div>
             </div>
+            @endguest
 
             @auth
             <!-- Settings Dropdown -->
