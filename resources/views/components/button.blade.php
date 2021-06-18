13f87fddd5e995c54d8547b0fcb5b5de6bbc990c
Sahil Bhatia <sahilofficial671@gmail.com>
2021-06-18 21:19:33 +0530

    Button component dynamics

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,14 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props(['buttonType' => null])
+
+@php
+    $class = 'inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-500 disabled:opacity-25 transition ease-in-out duration-150';
+
+    if($buttonType == "secondary"){
+        $class .= " bg-indigo-100 hover:bg-indigo-200 text-indigo-700 ring-indigo-200 focus:border-indigo-300";
+    }
+@endphp
+
+
+<button {{ $attributes->merge(['class' => $class]) }}>
     {{ $slot }}
 </button>
