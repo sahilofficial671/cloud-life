bb320adc60a7a38b6d28e24f1c728b298f8e07ec
Sahil Bhatia <sahilofficial671@gmail.com>
2021-06-19 17:52:06 +0530

    Added light button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -12,6 +12,7 @@
         'primary'   => ' bg-green-600 hover:bg-green-700 text-white ring-green-200 focus:border-green-700 active:bg-green-700',
         'secondary' => ' bg-indigo-100 hover:bg-indigo-200 text-indigo-700 ring-indigo-200 focus:border-indigo-300 active:bg-indigo-900',
         'danger'    => ' bg-red-600 hover:bg-red-700 text-white ring-red-200 focus:border-red-700 active:bg-red-700',
+        'light'     => ' bg-gray-200 hover:bg-gray-300 text-gray-600 ring-gray-200 focus:border-gray-300 active:bg-gray-300',
     ][$buttonType];
 
 @endphp
