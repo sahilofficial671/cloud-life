7240db9ee5c6f8cb9b365ba8e17a5a75b8372494
Sahil Bhatia <sahilofficial671@gmail.com>
2021-06-21 15:16:20 +0530

    removed unwanted space from textarea

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -2,6 +2,4 @@
     'disabled' => false
 ])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50']) !!}>
-    {{$slot}}
-</textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50']) !!}>{{$slot}}</textarea>
