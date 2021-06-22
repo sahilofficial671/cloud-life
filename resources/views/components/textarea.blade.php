b8381e12f3d8849d43cfa8b827c0c354a0043748
Sahil Bhatia <sahilofficial671@gmail.com>
2021-06-22 23:02:35 +0530

    added textarea value property as slot

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,5 +1,6 @@
 @props([
-    'disabled' => false
+    'disabled' => false,
+    'value' => null,
 ])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50']) !!}>{{$slot}}</textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50']) !!}>{{ $value ?? ''}}{{$slot}}</textarea>
