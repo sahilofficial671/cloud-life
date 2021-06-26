5aa4278ab7fe5c44d07769569fddd2f96f4650c4
Sahil Bhatia <sahilofficial671@gmail.com>
2021-06-26 12:28:26 +0530

    Light button classes changed

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,12 +1,14 @@
 @props([
     'buttonType' => 'primary',
+    'type'       => 'button',
     'withIcon'   => false,
     'height'     => 'h-10',
     'disabled'   => false,
+    'value'      => '',
 ])
 
 @php
-    $class = 'inline-flex py-2 items-center border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring disabled:opacity-25 transition ease-in-out duration-150 shadow-sm ';
+    $class = 'inline-flex py-2 items-center border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring disabled:opacity-25 transition ease-in-out duration-150 shadow-sm';
 
     $class .= ' '.$height;
 
@@ -17,7 +19,7 @@
         'secondary'       => ' bg-indigo-100 border-indigo-200 border-opacity-60 text-indigo-700 ring-indigo-200 focus:border-indigo-300 active:bg-indigo-900',
         'danger'          => ' bg-red-600 text-white ring-red-200 focus:border-red-700 active:bg-red-700',
         'danger-light'    => ' bg-red-200 text-red-600 ring-red-200 focus:border-red-300 active:bg-red-300 border-red-300 border-opacity-60',
-        'light'           => ' bg-gray-200 text-gray-600 ring-gray-200 focus:border-gray-300 active:bg-gray-300',
+        'light'           => ' bg-white text-gray-600 ring-gray-200 focus:border-gray-300 active:bg-gray-300 border-gray-300',
         'light-success'   => ' bg-green-200 text-green-600 ring-green-200 focus:border-green-300 active:bg-green-300 border-green-300 border-opacity-60',
     ][$buttonType];
 
@@ -30,13 +32,13 @@
             'secondary'       => 'hover:bg-indigo-200',
             'danger'          => 'hover:bg-red-700',
             'danger-light'    => 'hover:bg-red-300',
-            'light'           => 'hover:bg-gray-300',
+            'light'           => 'hover:bg-gray-50',
             'light-success'   => 'hover:bg-green-300',
         ][$buttonType];
     }
 
 @endphp
 
-<button {{ $attributes->merge(['class' => $class, 'disabled' => $disabled]) }}>
-    {{ $slot }}
+<button type="{!! $type !!}" {{ $attributes->merge(['class' => $class, 'disabled' => $disabled]) }}>
+    {{ $slot }}{!! $value !!}
 </button>
