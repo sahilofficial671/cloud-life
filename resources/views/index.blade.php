7f4a6d93bf0989b0d3fff37e77968d511f51a85d
Sahil Bhatia <sahilofficial671@gmail.com>
2021-06-27 22:31:14 +0530

    Auth dashboard button added to home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,9 +11,17 @@
             </p>
             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div class="rounded-md shadow">
-                    <a href="{{route('login')}}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
-                        Get started
-                    </a>
+                    @guest
+                        <a href="{{ route('login') }}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+                            Get started
+                        </a>
+                    @endguest
+
+                    @auth
+                        <a href="{{ route('dashboard') }}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+                            Go to Dashboard
+                        </a>
+                    @endauth
                 </div>
             </div>
         </div>
