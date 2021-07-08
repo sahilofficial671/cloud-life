d9074f1d257957a1e680b8e54d19b8fa60d218b7
Sahil Bhatia <sahilofficial671@gmail.com>
2021-07-08 21:55:18 +0530

    Added light pill

--- resources/views/components/pill.blade.php
+++ resources/views/components/pill.blade.php
@@ -4,11 +4,12 @@
 ])
 
 @php
-    $class = 'text-xs font-semibold p-1 rounded-md';
+    $class = 'text-xs text-center font-semibold p-1 rounded-md';
 
     $class .= ' '.[
         'info'    => 'bg-indigo-100 text-indigo-600',
         'danger'  => 'bg-red-100 text-red-600',
+        'light'   => 'bg-gray-100 text-gray-600',
         'warning' => 'bg-yellow-100 text-yellow-600',
         'success' => 'bg-green-100 text-green-600',
     ][$type];
