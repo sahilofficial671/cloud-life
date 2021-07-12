76e198d67064d75d3e8f57c550efddb61e5355f2
Sahil Bhatia <sahilofficial671@gmail.com>
2021-07-12 22:20:18 +0530

    Added upcomming change view on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
 
                     <div class="md:w-1/2 lg:w-1/3 p-4">
                         <a href="{{ route('vehicles.index') }}">
-                            <x-card width="w-full sm:max-w-xs md:max-w-sm" class="mx-auto ring-1 ring-indigo-50 hover:ring-4 hover:ring-indigo-100 ring-opacity-75">
+                            <x-card width="w-full lg:h-32 sm:max-w-xs md:max-w-sm" class="mx-auto ring-1 ring-indigo-50 hover:ring-4 hover:ring-indigo-100 ring-opacity-75">
                                 <x-slot name="body">
                                     <div class="group">
                                         <div class="flex justify-between items-center">
@@ -31,6 +31,28 @@
                                 </x-slot>
                             </x-card>
                         </a>
+                    </div>
+
+                    <div class="md:w-1/2 lg:w-1/3 p-4">
+                        <div>
+                            <x-card width="w-full lg:h-32 sm:max-w-xs md:max-w-sm" class="mx-auto relative ring-1 ring-indigo-50 bg-gray-50">
+                                <x-slot name="body">
+                                    <div class="group">
+                                        <div class="flex justify-between items-center">
+                                            <div class="left w-3/4 px-4 sm:px-6 py-4">
+                                                <h2 class="text-lg text-gray-600 font-semibold title-font mb-1">Finance</h2>
+                                                <p class="leading-tight text-gray-400 text-sm sm:text-base">Manage your money like honey! <br /> <span class="text-sm">Income, Expenses, Loans, etc.</span> </p>
+                                                <x-pill value="Upcoming" class="absolute bottom-3 right-3" />
+                                            </div>
+                                            <div class="right w-1/4 text-center px-4 sm:px-6">
+                                                <div class="text-8xl lg:text-9xl font-semibold md:font-bold text-indigo-100 bg-opacity-70 -ml-4 transition-all duration-300 ease-in-out">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </x-slot>
+                            </x-card>
+                        </div>
                     </div>
 
                 </div>
